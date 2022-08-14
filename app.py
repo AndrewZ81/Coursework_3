f7aa62cc56bbf896ea48ce6ac123ca8c98a0ee56
Andrew Zaiko <andrewzaiko@gmail.com>
2022-08-14 13:34:59 +0300

    Создал блюпринт страницы поста с комментариями

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask  # Подключаем необходимые инструменты из модуля flask
 from config import FlaskConfig  # Подключаем конфигурационный класс
 from app.main.all_posts_views import all_posts_blueprint  # Подключаем блюпринт для вывода ленты
+from app.post.post_view import post_with_comments_blueprint # Подключаем блюпринт для вывода поста с комментариями
 
 app = Flask(__name__)  # Создаём наше приложение
 app.config.from_object(FlaskConfig)  # Подключаем для доступа к конфигурационным константам
-app.register_blueprint(all_posts_blueprint)  # Регистрируем блюпринты
+
+# Регистрируем блюпринты
+app.register_blueprint(all_posts_blueprint)
+app.register_blueprint(post_with_comments_blueprint)
 
 if __name__ == "__main__":
     app.run()
